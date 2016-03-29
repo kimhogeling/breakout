391f99a88e67451a6e65e75bf2eeb8de5003208d
Kim Hogeling <kim.hogeling@s24.com>
2016-03-29 23:01:39 +0200

    adjust window size, prevent scroll bars

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 320, height: 500});
+  mainWindow = new BrowserWindow({width: 320, height: 510});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
